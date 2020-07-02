6d8ba9b03ce506007a3398ef1c6f71ad72af7e19
Doktrjegih <doktrjegih@gmail.com>
2020-07-02 12:01:29 +0500

    Тохапомогай

--- combo.py
+++ combo.py
@@ -16,11 +16,13 @@ def combo(list):
     if sorted(list) == poltorashka:
         scores += 1500
         a1 = False; a5 = False
+        list = []
 
     # проверка на 3 пары
     if sorted(counter_mass) == [0, 0, 0, 2, 2, 2]:
         scores += 750
         a1 = False; a5 = False
+        list = []
 
     # проверка на 3 и более одинаковых (словарем)
     b1 = dict()
@@ -68,18 +70,18 @@ def combo(list):
     if sorted(list) != poltorashka:
         ot_1_do_5 = 0
         for i in counter_mass[0:5]:
-            if i > 0:
+            if i > 0: # и если в сортированном счетчике не больше 1-й единицы или пятерки крч
                 ot_1_do_5 += 1
         if ot_1_do_5 == 5:
             scores += 500
-            a1= False; a5 = False
+            a1 = False; a5 = False
         ot_2_do_6 = 0
         for i in counter_mass[1:6]:
             if i > 0:
                 ot_2_do_6 += 1
         if ot_2_do_6 == 5:
             scores += 750
-            a1= False; a5 = False
+            a1 = False; a5 = False
 
     # проверка на 1 и 5
     if a1 == True:
