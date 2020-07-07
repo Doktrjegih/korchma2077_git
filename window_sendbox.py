964cf90e79ac6e623cc4f8c9a0e6071e39a266df
ObiiOne <danilov.anthony@icloud.com>
2020-07-08 01:18:13 +0500

    сделал кубы и выбор кубов

--- window_sendbox.py
+++ window_sendbox.py
@@ -9,32 +9,46 @@ def roll_cubes():
         buffer = random.randint(1, 6)
         dice.append(buffer)
     print(dice)
+    cube_1.configure(text=dice[0])
+    cube_2.configure(text=dice[1])
+    cube_3.configure(text=dice[2])
+    cube_4.configure(text=dice[3])
+    cube_5.configure(text=dice[4])
+    cube_6.configure(text=dice[5])
+
+def cube_choice(event):
+    messagebox.showinfo("cube", "cube selected")
 
 root: Tk = Tk()
 # Описание параметров окна
 root.title("CyberKorchma 2077")
 root.geometry('320x240')
 root.resizable(False, False)
-root.configure(bg="gray")
 
 # кубы внутри окна
-cube_1 = Label(root, height=3, width=6, text="1",)
+cube_1 = Label(root, height=3, width=6, text="0",)
 cube_1.grid(row=1, column=1)
+cube_1.bind("<Button-1>", cube_choice)
 
-cube_2 = Label(root, height=3, width=6, text="2")
+cube_2 = Label(root, height=3, width=6, text="0")
 cube_2.grid(row=2, column=1)
+cube_2.bind("<Button-1>", cube_choice)
 
-cube_3 = Label(root, height=3, width=6, text="3")
+cube_3 = Label(root, height=3, width=6, text="0")
 cube_3.grid(row=3, column=1)
+cube_3.bind("<Button-1>", cube_choice)
 
-cube_4 = Label(root, height=3, width=6, text="4")
+cube_4 = Label(root, height=3, width=6, text="0")
 cube_4.grid(row=1, column=3)
+cube_4.bind("<Button-1>", cube_choice)
 
-cube_5 = Label(root, height=3, width=6, text="5")
+cube_5 = Label(root, height=3, width=6, text="0")
 cube_5.grid(row=2, column=3)
+cube_5.bind("<Button-1>", cube_choice)
 
-cube_6 = Label(root, height=3, width=6, text="6")
+cube_6 = Label(root, height=3, width=6, text="0")
 cube_6.grid(row=3, column=3)
+cube_6.bind("<Button-1>", cube_choice)
 
 #  тексты внутри окна
 global_score = Label(root, text="global score:")
@@ -62,7 +76,3 @@ button_end.grid(row=4, column=5)
 
 root.mainloop()
 
-необходимо научить функцию клик записывать бросок в лейблы кубов.
-тогда будет реализован первый бросок визуально, что дост понимание в каком
-направлении двигаться дальше.
-после нужно будет реализовать счетчик кубов и счетчик очков хода, а так же функцию конца хода.
