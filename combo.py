1ccb53ca2a6bbdadf91f184dae64b0cb8c1729bc
Doktrjegih <doktrjegih@gmail.com>
2020-07-09 18:49:17 +0500

    Rid of global variables and minor fixes of syntax

--- combo.py
+++ combo.py
@@ -1,12 +1,13 @@
-poltorashka = [1, 2, 3, 4, 5, 6]
+one_and_a_half = [1, 2, 3, 4, 5, 6]
 
-def combo(list):
-    global scores
+
+def first_combo(list1):
     scores = 0
-    a1 = True; a5 = True
+    a1 = True
+    a5 = True
 
     counter_mass = [0] * 6
-    for i in list:
+    for i in list1:
         count = 0
         for j in range(6):
             count += 1
@@ -14,7 +15,7 @@ def combo(list):
                 counter_mass[i - 1] += 1
 
     # проверка на 1500
-    if sorted(list) == poltorashka:
+    if sorted(list1) == one_and_a_half:
         scores += 1500
         a1, a5 = False, False
 
@@ -25,9 +26,9 @@ def combo(list):
 
     # проверка на 3 и более одинаковых (словарем)
     b1 = dict()
-    for i in list:
+    for i in list1:
         count = 0
-        for j in list:
+        for j in list1:
             if i == j:
                 count += 1
                 b1[i] = count
@@ -66,7 +67,7 @@ def combo(list):
             a5 = False
 
     # ебаная проверка КК (а можно бы было втупую сравнить с листом 1-5 или 2-6)
-    if sorted(list) != poltorashka:
+    if sorted(list1) != one_and_a_half:
         ot_1_do_5 = 0
         for i in counter_mass[0:5]:
             if i > 0:
@@ -85,19 +86,124 @@ def combo(list):
         if ot_2_do_6 == 5:
             scores += 750
             a1, a5 = False, False
-            if b1[1] == 2:      # это нахуй не надо, потому что это 1500 сразу
-                scores += 100   # это нахуй не надо, потому что это 1500 сразу
             if b1[5] == 2:
                 scores += 50
 
     # проверка на 1 и 5
-    if a1 == True:
-        for i in list:
+    if a1 is True:
+        for i in list1:
             if i == 1:
                 scores += 100
-    if a5 == True:
-        for i in list:
+    if a5 is True:
+        for i in list1:
             if i == 5:
                 scores += 50
 
-    print(scores)
+    return scores
+
+
+def combo(list1):
+    scores = 0
+    a1 = True
+    a5 = True
+
+    def del_cube(cube):
+        del (list1[list1.index(cube)])
+
+    counter_mass = [0] * 6
+    for i in list1:
+        count = 0
+        for j in range(6):
+            count += 1
+            if i == count:
+                counter_mass[i - 1] += 1
+
+    # проверка на 1500
+    if sorted(list1) == one_and_a_half:
+        scores += 1500
+        a1, a5 = False, False
+
+    # проверка на 3 пары
+    if sorted(counter_mass) == [0, 0, 0, 2, 2, 2]:
+        scores += 750
+        a1, a5 = False, False
+
+    # проверка на 3 и более одинаковых (словарем)
+    b1 = dict()
+    for i in list1:
+        count = 0
+        for j in list1:
+            if i == j:
+                count += 1
+                b1[i] = count
+    for k in b1:
+        if b1[k] == 3 and k != 1 and k != 5:
+            scores += k * 100
+        elif b1[k] == 4 and k != 1 and k != 5:
+            scores += k * 200
+        elif b1[k] == 5 and k != 1 and k != 5:
+            scores += k * 400
+        elif b1[k] == 6 and k != 1 and k != 5:
+            scores += k * 800
+        elif b1[k] == 3 and k == 1:
+            scores += 1000
+            a1 = False
+        elif b1[k] == 4 and k == 1:
+            scores += 2000
+            a1 = False
+        elif b1[k] == 5 and k == 1:
+            scores += 4000
+            a1 = False
+        elif b1[k] == 6 and k == 1:
+            scores += 8000
+            a1 = False
+        elif b1[k] == 3 and k == 5:
+            scores += 500
+            a5 = False
+        elif b1[k] == 4 and k == 5:
+            scores += 1000
+            a5 = False
+        elif b1[k] == 5 and k == 5:
+            scores += 2000
+            a5 = False
+        elif b1[k] == 6 and k == 5:
+            scores += 4000
+            a5 = False
+
+    # ебаная проверка КК (а можно бы было втупую сравнить с листом 1-5 или 2-6)
+    if sorted(list1) != one_and_a_half:
+        ot_1_do_5 = 0
+        for i in counter_mass[0:5]:
+            if i > 0:
+                ot_1_do_5 += 1
+        if ot_1_do_5 == 5:
+            scores += 500
+            a1, a5 = False, False
+            if b1[1] == 2:
+                scores += 100
+            if b1[5] == 2:
+                scores += 50
+        ot_2_do_6 = 0
+        for i in counter_mass[1:6]:
+            if i > 0:
+                ot_2_do_6 += 1
+        if ot_2_do_6 == 5:
+            scores += 750
+            a1, a5 = False, False
+            if b1[5] == 2:
+                scores += 50
+
+    # проверка на 1 и 5
+    if a1 is True:
+        for i in list1:
+            if i == 1:
+                scores += 100
+                del_cube(i)
+    if a5 is True:
+        for i in list1:
+            if i == 5:
+                scores += 50
+                del_cube(i)
+
+    print('лист после подсчета комбо: ', list1)
+    return scores
