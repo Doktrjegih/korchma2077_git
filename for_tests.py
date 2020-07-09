b477a1b09f5c232946f12f94b8689ffd48f995fd
Doktrjegih <doktrjegih@gmail.com>
2020-07-10 01:48:08 +0500

    Full cycle roll + counter of score

--- for_tests.py
+++ for_tests.py
@@ -1,14 +1,20 @@
 import random
+import combo as cb
 
 def roll():
     a = 6
+    scr_on_turn = 0
     while a > 0:
         d = list()
-        for j in range(a):
+        for j in range(a):  # бросок костей
             b = random.randint(1, 6)
             d.append(b)
         print('сформирован лист броска - ', d)
-        d2 = list()
+        c = cb.first_combo(d)
+        if c == 0:  # проверка, есть ли комбо в броске
+            print('нет комбо')
+            break
+        d2 = list()  # откладывание костей
         print('введите номера костей, которые нужно отложить:')
         d1 = list(input())
         for i in d1:
@@ -16,9 +22,13 @@ def roll():
                 break
             else:
                 d2.append(d[int(i) - 1])
-        print('лист отложенных костей:', d2)
-        a -= len(d2)
-        print(a)
+        scr_on_turn = scr_on_turn + cb.first_combo(d2)
+        print('лист отложенных костей:', d2, ' подытог хода -', scr_on_turn)
+        e = input('кидаем снова? (Y/N): ')
+        if e == 'Y' or e == 'y':
+            a -= len(d2)
+        if e == 'N' or e == 'n':
+            break
 
 
 roll()
