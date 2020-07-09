ab9ac3d35267b6bd6bdfcfc032576453c805947d
Doktrjegih <doktrjegih@gmail.com>
2020-07-09 19:55:23 +0500

    Added combo_tips function, cleared main.py

--- combo.py
+++ combo.py
@@ -68,22 +68,22 @@ def first_combo(list1):
 
     # ебаная проверка КК (а можно бы было втупую сравнить с листом 1-5 или 2-6)
     if sorted(list1) != one_and_a_half:
-        ot_1_do_5 = 0
+        from_1_to_5 = 0
         for i in counter_mass[0:5]:
             if i > 0:
-                ot_1_do_5 += 1
-        if ot_1_do_5 == 5:
+                from_1_to_5 += 1
+        if from_1_to_5 == 5:
             scores += 500
             a1, a5 = False, False
             if b1[1] == 2:
                 scores += 100
             if b1[5] == 2:
                 scores += 50
-        ot_2_do_6 = 0
+        from_2_to_6 = 0
         for i in counter_mass[1:6]:
             if i > 0:
-                ot_2_do_6 += 1
-        if ot_2_do_6 == 5:
+                from_2_to_6 += 1
+        if from_2_to_6 == 5:
             scores += 750
             a1, a5 = False, False
             if b1[5] == 2:
@@ -172,22 +172,22 @@ def combo(list1):
 
     # ебаная проверка КК (а можно бы было втупую сравнить с листом 1-5 или 2-6)
     if sorted(list1) != one_and_a_half:
-        ot_1_do_5 = 0
+        from_1_to_5 = 0
         for i in counter_mass[0:5]:
             if i > 0:
-                ot_1_do_5 += 1
-        if ot_1_do_5 == 5:
+                from_1_to_5 += 1
+        if from_1_to_5 == 5:
             scores += 500
             a1, a5 = False, False
             if b1[1] == 2:
                 scores += 100
             if b1[5] == 2:
                 scores += 50
-        ot_2_do_6 = 0
+        from_2_to_6 = 0
         for i in counter_mass[1:6]:
             if i > 0:
-                ot_2_do_6 += 1
-        if ot_2_do_6 == 5:
+                from_2_to_6 += 1
+        if from_2_to_6 == 5:
             scores += 750
             a1, a5 = False, False
             if b1[5] == 2:
@@ -207,3 +207,65 @@ def combo(list1):
 
     print('лист после подсчета комбо: ', list1)
     return scores
+
+
+def tips_combo(list1):  # ПРОВЕРКИ КОМБИНАЦИЙ (без счета)
+    # счетчик кубов циклом
+    counter_mass = [0] * 6
+    for i in list1:
+        count = 0
+        for j in range(6):
+            count += 1
+            if i == count:
+                counter_mass[i - 1] += 1
+    # print('счетчик кубов (сортированный список): ', counter_mass)
+
+    # проверка на 3 и более одинаковых (словарем)
+    b1 = dict()
+    for i in list1:
+        count = 0
+        for j in list1:
+            if i == j:
+                count += 1
+                b1[i] = count
+    # print('счетчик кубов (несортированный словарь): ', b1)
+    for k in b1:
+        if b1[k] == 3:
+            print('троечька')
+        elif b1[k] == 4:
+            print('четыре, неплохо')
+        elif b1[k] == 5:
+            print('пятерка, воу!')
+        elif b1[k] == 6:
+            print('ДЖЕКПОТ!!!')
+
+    # проверка на 1 и 5
+    for i in list1:
+        if i == 1 or i == 5:
+            print('в кубах есть 1 или 5!')
+            break
+
+    # проверка на 3 пары
+    if sorted(counter_mass) == [0, 0, 0, 2, 2, 2]:
+        print('три пары: 750 очков и новый бросок')
+
+    # проверка на 1500
+    if sorted(list1) == one_and_a_half:
+        print('полторашка!')
+
+    # ебаная проверка КК
+    # print(counter_mass[0:5], ' - от 1 до 5 должны быть > 0')
+    # print(counter_mass[1:6], ' - от 2 до 6 должны быть > 0')
+    from_1_to_5 = 0
+    for i in counter_mass[0:5]:
+        if i > 0:
+            from_1_to_5 += 1
+    if from_1_to_5 == 5:
+        print('1-5 комбо, 500 очков!')
+
+    from_2_to_6 = 0
+    for i in counter_mass[1:6]:
+        if i > 0:
+            from_2_to_6 += 1
+    if from_2_to_6 == 5:
+        print('2-6 комбо, 750 очков!')
--- main.py
+++ main.py
@@ -3,90 +3,23 @@
 import random
 import combo as cb
 
+test_p = [1, 3, 1, 5, 4, 1]  # used for tests
 
 # first roll
 d = list()
 for i in range(6):
     b = random.randint(1, 6)
     d.append(b)
-print('сформирован лист броска - ', d)
-
-
-test_p = [2, 5, 3, 4, 6, 6]  # used for tests
+print('сформирован лист броска - ', test_p)
 
 
 # проверка на комбо в первом броске
-first_roll = cb.first_combo(d)
+first_roll = cb.first_combo(test_p)
 if first_roll == 0:
     print('пока')
     exit(0)
-
-
-# счетчик кубов циклом
-counter_mass = [0] * 6
-for i in d:
-    count = 0
-    for j in range(6):  # range(len(a)) ?
-        count += 1
-        if i == count:
-            counter_mass[i-1] += 1
-# print('счетчик кубов (сортированный список): ', counter_mass)
-
-
-# --------------<ПРОВЕРКИ КОМБИНАЦИЙ (без счета)>-------------- #
-
-# проверка на 3 и более одинаковых (словарем)
-b1 = dict()
-for i in d:
-    count = 0
-    for j in d:
-        if i == j:
-            count += 1
-            b1[i] = count
-# print('счетчик кубов (несортированный словарь): ', b1)
-for k in b1:
-    if b1[k] == 3:
-        print('троечька')
-    elif b1[k] == 4:
-        print('четыре, неплохо')
-    elif b1[k] == 5:
-        print('пятерка, воу!')
-    elif b1[k] == 6:
-        print('ДЖЕКПОТ!!!')
-
-# проверка на 1 и 5
-for i in d:
-    if i == 1 or i == 5:
-        print('в кубах есть 1 или 5!')
-        break
-
-# проверка на 3 пары
-if sorted(counter_mass) == [0, 0, 0, 2, 2, 2]:
-    print('три пары: 750 очков и новый бросок')
-
-# проверка на 1500
-poltorashka = [1, 2, 3, 4, 5, 6]
-if sorted(d) == poltorashka:
-    print('полторашка!')
-
-# ебаная проверка КК
-# print(counter_mass[0:5], ' - от 1 до 5 должны быть > 0')
-# print(counter_mass[1:6], ' - от 2 до 6 должны быть > 0')
-ot_1_do_5 = 0
-for i in counter_mass[0:5]:
-    if i > 0:
-        ot_1_do_5 += 1
-if ot_1_do_5 == 5:
-    print('1-5 комбо, 500 очков!')
-
-ot_2_do_6 = 0
-for i in counter_mass[1:6]:
-    if i > 0:
-        ot_2_do_6 += 1
-if ot_2_do_6 == 5:
-    print('2-6 комбо, 750 очков!')
-
-# --------------</ПРОВЕРКИ КОМБИНАЦИЙ (без счета)>-------------- #
+else:
+    print(first_roll)
 
 
 # лист отложенных костей - название для Виталика?)))
@@ -97,8 +30,15 @@ for i in d1:
     if int(i) < 1:
         break
     else:
-        d2.append(d[int(i)-1])
+        d2.append(test_p[int(i)-1])
 print('лист отложенных костей:', d2)
 
 
 # проверка отложенного листа
+roll = cb.combo(test_p)
+if roll == 0:
+    print('Нет комбо')
+else:
+    print(roll)
+
+print(cb.one_and_a_half)
