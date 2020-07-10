76aa1ed936c76e03363b3fb6a0dc4de51fe1ddea
Doktrjegih <doktrjegih@gmail.com>
2020-07-10 18:08:08 +0500

    Добавил камбэк и пояснения к коду

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def roll():
         d2 = list()
         for i in d1:
             d2.append(d[int(i) - 1])
-        scr_on_turn = scr_on_turn + cb.combo(d2)  # счетчик очков за ход
+        scr_on_turn = scr_on_turn + cb.combo(d2)  # подсчет очков за ход
         print('лист отложенных костей:', d2, ' подытог хода -', scr_on_turn)
         e = input('кидаем снова? (+/-): ')  # запрос нового броска
         a -= len(d2)  # уменьшение кол-ва кубов в руке
@@ -34,11 +34,10 @@ def roll():
             break
         if a == 0:
             a = 6
-    return scr_on_turn
+    return scr_on_turn  # возврат функции
 
 
-def whoGoesFirst():
-    # Случайный выбор игрока, который ходит первым.
+def who_goes_first():  # случайный выбор игрока, который ходит первым
     if random.randint(0, 1) == 0:
         return 'Саня'
     else:
@@ -47,28 +46,62 @@ def whoGoesFirst():
 
 score1 = 0
 score2 = 0
-turn = whoGoesFirst()
+turn = who_goes_first()  # первый ход тому, кто выбран в ф-ии who_goes_first
 print('' + turn + ' ходит первым')
 gameIsPlaying = True
 
 while gameIsPlaying:
+
     if turn == 'Человек':
-        print('========Ход Человека========')
-        a22 = roll()
-        score1 = score1 + a22
-        print('Общие очки Человека =', score1)
-        if score1 > 1000:
-            print('Человек победил!')
-            gameIsPlaying = False
+        if score2 > 300:  # проверка на победу второго игрока
+            print('========Последний ход Человека========')
+            a22 = roll()  # роллим и берем результат ф-ии
+            score1 = score1 + a22  # считаем общие очки
+            if score1 > score2:  # проверка на камбэк
+                print('КАМБЭК! Человек победил!')
+                print('Общие очки Человека =', score1)
+                print('Общие очки Сани =', score2)
+                gameIsPlaying = False
+            else:
+                print('Увы, Саня победил')
+                print('Общие очки Сани =', score2)
+                print('Общие очки Человека =', score1)
+                gameIsPlaying = False
         else:
-            turn = 'Саня'
+            print('========Ход Человека========')
+            a22 = roll()
+            score1 = score1 + a22
+            print('Общие очки Человека =', score1)
+
+            if score1 > 300:
+                print('Последний шанс Сани!')
+                turn = 'Саня'
+            else:
+                turn = 'Саня'
+
     if turn == 'Саня':
-        print('========Ход Сани========')
-        b22 = roll()
-        score2 = score2 + b22
-        print('Общие очки Сани =', score2)
-        if score2 > 1000:
-            print('Саня победил!')
-            gameIsPlaying = False
+        if score1 > 300:
+            print('========Последний ход Сани========')
+            a22 = roll()
+            score1 = score1 + a22
+            if score2 > score1:
+                print('КАМБЭК! Саня победил!')
+                print('Общие очки Сани =', score2)
+                print('Общие очки Человека =', score1)
+                gameIsPlaying = False
+            else:
+                print('Увы, Человек победил')
+                print('Общие очки Человека =', score1)
+                print('Общие очки Сани =', score2)
+                gameIsPlaying = False
         else:
-            turn = 'Человек'
+            print('========Ход Сани========')
+            b22 = roll()
+            score2 = score2 + b22
+            print('Общие очки Сани =', score2)
+
+            if score1 > 300:
+                print('Последний шанс Человека')
+                turn = 'Человек'
+            else:
+                turn = 'Человек'
